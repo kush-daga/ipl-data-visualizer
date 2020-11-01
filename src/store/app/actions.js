fba6f4924e01b90d09ba02790223ae20ef2eb301
Kush Daga <kushdaga@hotmail.com>
2020-11-01 18:07:40 +0530

    fixed

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -2,37 +2,37 @@ import { FETCH_DATA, DATA_FETCH_SUCCESS, DATA_FETCH_FAIL } from "./types";
 import axios from "axios";
 
 export const fetchData = () => (dispatch) => {
-  const config = {
-    headers: {
-      "Content-type": "application/json",
-    },
-  };
+	const config = {
+		headers: {
+			"Content-type": "application/json",
+		},
+	};
 
-  // const url = `https://ipl-data-atlan.herokuapp.com/db`;
-  const url = `http://localhost:5000/db`;
+	const url = `https://ipl-data-atlan.herokuapp.com/db`;
+	// const url = `http://localhost:5000/db`;
 
-  dispatch({
-    type: FETCH_DATA,
-  });
+	dispatch({
+		type: FETCH_DATA,
+	});
 
-  axios
-    .get(url, config)
-    .then((res) => {
-      dispatch({
-        type: DATA_FETCH_SUCCESS,
-        payload: res.data.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: DATA_FETCH_FAIL,
-        payload: err,
-      });
-    });
+	axios
+		.get(url, config)
+		.then((res) => {
+			dispatch({
+				type: DATA_FETCH_SUCCESS,
+				payload: res.data.data,
+			});
+		})
+		.catch((err) => {
+			dispatch({
+				type: DATA_FETCH_FAIL,
+				payload: err,
+			});
+		});
 };
 
 const appActions = {
-  fetchData,
+	fetchData,
 };
 
 export default appActions;
