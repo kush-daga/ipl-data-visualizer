0555cb0c6048d2c6f18a7559d784101f418afd56
Kush Daga <kushdaga@hotmail.com>
2020-11-01 19:19:34 +0530

    Made offline usable with redux persist

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/App/index.jsx";
 import * as serviceWorker from "./serviceWorker";
 import GlobalStyles from "./styles/globalStyles.js";
-import store from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+
 import { Provider } from "react-redux";
 ReactDOM.render(
-  <Provider store={store}>
-    <GlobalStyles />
-    <App />
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<PersistGate loading={() => <h1>Loading</h1>} persistor={persistor}>
+			<GlobalStyles />
+			<App />
+		</PersistGate>
+	</Provider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 import rootReducer from "./rootReducer";
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)) //Redux thunk
+	persistedReducer,
+	composeWithDevTools(applyMiddleware(thunk)) //Redux thunk
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
